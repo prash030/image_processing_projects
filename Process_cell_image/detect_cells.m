06b98eabbb511c2e39408b88609b4d90031e2506
Prash Ganesan <43354782+cardyfib@users.noreply.github.com>
2019-11-21 23:41:39 -0800

    Add files via upload

---
+++ Process_cell_image/detect_cells.m
@@ -0,0 +1,28 @@
+% Read the rgb image
+I = imread('original.jpg');
+figure;imshow(I)
+
+% Convert RGB to gray
+I_gray = rgb2gray(I);
+figure; imshow(I_gray);
+
+% Detecting the cells
+I_copy=I_gray;
+[N,edges] = histcounts(double(I_copy(:)),10);
+[~,ind] = max(N);
+threshold = edges(ind-1);
+I_copy=I_copy<threshold;
+figure; imshow(I_copy);
+
+[B,L] = bwboundaries(I_copy,'noholes');
+hold on
+areamat=[];
+for k = 1:length(B)
+   boundary = B{k};
+   areamat(k) = polyarea(boundary(:,2), boundary(:,1));
+   plot(boundary(:,2), boundary(:,1), 'r', 'LineWidth', 2)
+   if areamat(k)>2500 % hardcoded because the cell size doesnt change much
+       plot(boundary(:,2), boundary(:,1), 'g', 'LineWidth', 2)
+   end
+end
+
